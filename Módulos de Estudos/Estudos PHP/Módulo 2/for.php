e8df8a3e0c155967d1cb53ca4e83e706e5a4e716
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-09-26 12:17:47 -0300

    loop for

---
+++ Módulos de Estudos/Estudos PHP/Módulo 2/for.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+tudo igual :
+
+$numero++;
+$numero += 1;
+$numero = $numero +1;
+*/
+
+
+for ($numero= 0; $numero <  10; $numero++) { 
+    echo "Número: ".$numero."<br/>";
+}
+
+
+?>
